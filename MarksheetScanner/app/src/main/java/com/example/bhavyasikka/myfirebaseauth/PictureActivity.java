f2edc74e6d17c1c1e9396417b29e69c884bced06
Devashish Lal <laldevashish@gmail.com>
2019-04-17 02:04:23 +0530

    Image upload implemented

--- MarksheetScanner/app/src/main/java/com/example/bhavyasikka/myfirebaseauth/PictureActivity.java
+++ MarksheetScanner/app/src/main/java/com/example/bhavyasikka/myfirebaseauth/PictureActivity.java
@@ -1,23 +1,34 @@
 package com.example.bhavyasikka.myfirebaseauth;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.icu.text.SimpleDateFormat;
 import android.net.Uri;
+import android.os.Debug;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.NavUtils;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.bhavyasikka.myfirebaseauth.Network.Api;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -27,8 +38,19 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Locale;
+
+import retrofit2.Call;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class PictureActivity extends AppCompatActivity {
 
@@ -150,15 +172,49 @@ public class PictureActivity extends AppCompatActivity {
                 .child(new_course_code_id).child(new_exam_id);
 
 
-        save_new_photo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        save_new_photo.setOnClickListener(view -> {
 
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.setTitle("Roll No.");
+
+            final EditText input = new EditText(this);
+
+            input.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+            builder.setView(input);
+
+            builder.setPositiveButton("OK", (dialog, which) -> {
+                String rollno = input.getText().toString();
+                Log.d("ALERT DIAGOL", rollno);
                 Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-                startActivityForResult(intent,CAMERA_REQUEST_CODE);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    // Create the File where the photo should go
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile(rollno);
+                        Log.d("FILE CREATOR", photoFile.getName());
+                    } catch (IOException ex) {
+                        // Error occurred while creating the File
+                        ex.printStackTrace();
+                    }
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        Uri photoURI = FileProvider.getUriForFile(this,
+                                "com.example.android.fileprovider",
+                                photoFile);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(intent, CAMERA_REQUEST_CODE);
+                    }
+                }
+            });
 
-            }
+            builder.setNegativeButton("Cancel", (dialog, which) -> {
+                dialog.cancel();
+            });
+
+            builder.show();
         });
 
 
@@ -170,66 +226,43 @@ public class PictureActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode==CAMERA_REQUEST_CODE && resultCode== RESULT_OK) {
+        if(requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
 
-            mprogressDialog.setMessage("uploading...");
-            mprogressDialog.show();
+            File imageFile = new File(currentPhotoPath);
 
-              //uri=data.getData();
-                //code to get camera image
-            Bundle extras = data.getExtras();
-            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-            byte[] dataBAOS = baos.toByteArray();
+            RequestBody reuqestFile = RequestBody.create(MediaType.parse("multipart/form-data"), imageFile);
 
-            //name of the image file (add time to have different files to avoid rewrite on the same file)
+            MultipartBody.Part body = MultipartBody.Part.createFormData("upload", imageFile.getName(), reuqestFile);
 
-            StorageReference imagesRef = mStorageReference.child("filename");
-                    //+ new Date().getTime());
+            Call<ResponseBody> call = Api.Service().submitResult(body, reuqestFile);
 
-            //upload image
-
-            UploadTask uploadTask = imagesRef.putBytes(dataBAOS);
-            uploadTask.addOnFailureListener(new OnFailureListener() {
+            call.enqueue(new Callback<ResponseBody>() {
                 @Override
-                public void onFailure(@NonNull Exception exception) {
-                    Toast.makeText(getApplicationContext(),"Sending failed", Toast.LENGTH_SHORT).show();
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                    Toast.makeText(getApplicationContext(), "File Uploaded Successfully...", Toast.LENGTH_LONG).show();
                 }
-            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
                 @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
-
-            //handle success
-
-                    Toast.makeText(getApplicationContext(),"Sent", Toast.LENGTH_SHORT).show();
-                    mprogressDialog.dismiss();
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
+                    Log.e("NETWORK STATE", "Ohh Noo!!!");
+                    t.printStackTrace();
                 }
             });
         }
-
-           /* StorageReference filepath=mStorageReference.child("Photos").child(uri.getLastPathSegment());
-
-            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-                    mprogressDialog.dismiss();
-
-                    Toast.makeText(PictureActivity.this,"upload done!",Toast.LENGTH_SHORT).show();
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-
-                    mprogressDialog.dismiss();
-                    Toast.makeText(PictureActivity.this,"can't upload"+ e.getMessage(),Toast.LENGTH_LONG).show();
-                }
-            });
-        }*/
     }
 
+    String currentPhotoPath;
+    private File createImageFile(String imageFileName) throws IOException {
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        imageFileName += "_";
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
 
-
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 }
